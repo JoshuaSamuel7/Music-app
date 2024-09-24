3b3c7b066d22ceb5134e497aa8bc2e176c07e851
joshuasam7 <joshuasam365@gmail.com>
2024-09-24 20:27:52 +0530

    otp authentication with jwt

--- server/config/nodemailer.js
+++ server/config/nodemailer.js
@@ -1,9 +1,12 @@
-const nodemailer=require("nodemailer");
-const transporter=nodemailer.createTransport({
-    service:'Outlook365',
-    auth:{
-        user:process.env.EMAIL_USER,
-        pass:process.env.EMAIL_PASSWORD
-    }
-})
-module.exports=transporter;
+const nodemailer = require("nodemailer");
+const transporter = nodemailer.createTransport({
+    service: "Gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASSWORD,
+    },
+});
+module.exports = transporter;
